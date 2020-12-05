abcf043772c21bc91bab97fb87c362638f78f2c6
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2020-12-05 13:33:35 +0300

    WebHook

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import telebot
 from os import environ
 from dotenv import load_dotenv
+from flask import Flask, request
+
 
 # загружаем переменные окружения из файла .env
 load_dotenv()
 TOKEN = environ.get('TOKEN')
+PORT = environ.get('PORT', 5000)
 
-# создаем обьект bot
+# создаем обьект bot и обьект server
 bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
 
 
 # обработчик сообщений, который на любое текстовое сообщение от пользователя
@@ -17,10 +21,23 @@ def say_hi(message):
     bot.send_message(message.chat.id, 'Привет, я бот')
 
 
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-app-hse.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
 # main функция с polling
 # наш бот постоянно бегает на сервер Telegram с вопросом "Для меня есть сообщения?"
 def main():
-    bot.polling(none_stop=True)
+    server.run(host="0.0.0.0", port=PORT)
 
 
 if __name__ == '__main__':
